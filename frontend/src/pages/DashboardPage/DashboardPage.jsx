91f3d556d06c0d7eedc9222ce25e85e486100800
Yago Marialva Bentes <yago_bentes@calcomp-icct@org.br>
2024-07-02 17:05:36 -0400

    Adding charts in project

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,71 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
 
-const DashboardPage = () => {
-    const [comments, setComments] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [todos, setTodos] = useState([]);
+class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
 
-    useEffect(() => {
-        const fetchComments = async () => {
-            try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-                const data = await response.json();
-                setComments(data);
-            } catch (error) {
-                console.error('Error fetching comments:', error);
-            }
-        };
+    this.state = {
+      options: {
+        chart: {
+          id: "basic-bar"
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    };
+  }
 
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/users');
-                const data = await response.json();
-                setUsers(data);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        const fetchTodos = async () => {
-            try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-                const data = await response.json();
-                setTodos(data);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-        fetchTodos();
-        fetchUsers();
-        fetchComments();
-    }, []);
-
+  render() {
     return (
-        <Box sx={{ p: 3 }}>
-            <Typography variant="h4" gutterBottom>Dashboard</Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={6} lg={4}>
-                    <Paper sx={{ p: 2 }}>
-                        <Typography variant="h6">Total Comments</Typography>
-                        <Typography variant="body1">Number of comments: {comments.length}</Typography>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6} lg={4}>
-                    <Paper sx={{ p: 2 }}>
-                        <Typography variant="h6">Total Users</Typography>
-                        <Typography variant="body1">Number of users: {users.length}</Typography>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6} lg={4}>
-                    <Paper sx={{ p: 2 }}>
-                        <Typography variant="h6">Total Todos</Typography>
-                        <Typography variant="body1">Number of Todos: {todos.length}</Typography>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Box>
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
     );
-};
+  }
+}
 
 export default DashboardPage;
+
+
+// DashboardPage
