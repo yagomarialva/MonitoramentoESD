b12b5d97e9dc47e17d55c02036141778da585034
Yago Marialva Bentes <yago_bentes@calcomp-icct@org.br>
2024-10-04 10:48:03 -0400

    Refactorying menu componente

--- frontend/src/pages/Menu/Menu.tsx
+++ frontend/src/pages/Menu/Menu.tsx
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from "@mui/icons-material/Home";
-import PrecisionManufacturingOutlinedIcon from "@mui/icons-material/PrecisionManufacturingOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import SensorsOutlinedIcon from "@mui/icons-material/SensorsOutlined";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Layout, Menu, Typography, Button, Card, Spin } from "antd";
+import {
+  HomeOutlined,
+  UserOutlined,
+  SettingOutlined,
+  MonitorOutlined,
+  PlusOutlined,
+  DownOutlined,
+  UpOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import TranslateOutlinedIcon from '@mui/icons-material/TranslateOutlined';
+import Logo from "./logo-compal.png";
 import "./Menu.css";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import IconButton from "@mui/material/IconButton";
-import CircularProgress from "@mui/material/CircularProgress"; // Importa o indicador de progresso circular
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+const { Header, Sider, Content } = Layout;
 
 // Definição dos tipos para os dados do menu
 interface MenuItem {
@@ -43,10 +42,11 @@ const getMenuItems = (userRole: string): MenuItem[] => {
   const allItems: MenuItem[] = [
     {
       text: "Controle",
-      icon: <HomeIcon />,
+      icon: <HomeOutlined />,
       path: "/dashboard",
       roles: ["administrator", "operator"],
       subItems: [
+        { text: "Dashboard", path: "/dashboard" },
         { text: "Linhas", path: "/liners" },
         { text: "Estações", path: "/stations" },
         { text: "Ligar Estação e Linha", path: "/linkstationline" },
@@ -54,25 +54,25 @@ const getMenuItems = (userRole: string): MenuItem[] => {
     },
     {
       text: "Operadores",
-      icon: <AccountCircleOutlinedIcon />,
+      icon: <UserOutlined />,
       path: "/users",
       roles: ["administrator"],
     },
     {
       text: "Jigs",
-      icon: <PrecisionManufacturingOutlinedIcon />,
+      icon: <SettingOutlined />,
       path: "/jigs",
       roles: ["operator", "administrator"],
     },
     {
       text: "Monitores",
-      icon: <SensorsOutlinedIcon />,
+      icon: <MonitorOutlined />,
       path: "/monitors",
       roles: ["operator", "administrator"],
     },
     {
       text: "Cadastrar",
-      icon: <PersonAddIcon />,
+      icon: <PlusOutlined />,
       path: "/register",
       roles: ["administrator"],
     },
@@ -95,62 +95,30 @@ const MenuList: React.FC<MenuListProps> = ({ menuItems }) => {
     setExpandedItem(expandedItem === itemText ? null : itemText);
   };
 
-  useEffect(() => {
-    const currentItem = menuItems.find((item) =>
-      item.subItems?.some((subItem) => subItem.path === currentPath)
-    );
-    if (currentItem && expandedItem !== currentItem.text) {
-      setExpandedItem(currentItem.text);
-    }
-  }, [location, menuItems, expandedItem, currentPath]);
-
   const isSelected = (path: string) => currentPath === path;
 
   return (
-    <List>
+    <Menu mode="inline" theme="dark" defaultSelectedKeys={[currentPath]}>
       {menuItems.map((item, index) => (
-        <React.Fragment key={index}>
-          <ListItem disablePadding>
-            <ListItemButton
-              component={Link}
-              to={item.path}
-              className={`list-items-buttons ${
-                isSelected(item.path) ? "selected" : ""
-              }`}
-            >
-              <ListItemIcon className="list-items-buttons-icons">
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
-            {item.subItems && (
-              <IconButton
-                onClick={() => handleItemClick(item.text)}
-                edge="end"
-                sx={{ borderRadius: 0 }}
-              >
-                {expandedItem === item.text ? <ExpandLess /> : <ExpandMore />}
-              </IconButton>
-            )}
-          </ListItem>
-          {expandedItem === item.text && item.subItems && (
-            <List component="div" disablePadding className="drawer-sublist">
-              {item.subItems.map((subItem, subIndex) => (
-                <ListItem key={subIndex} disablePadding>
-                  <ListItemButton
-                    component={Link}
-                    to={subItem.path}
-                    className={isSelected(subItem.path) ? "selected" : ""}
-                  >
-                    <ListItemText primary={subItem.text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          )}
-        </React.Fragment>
+        <Menu.SubMenu
+          key={item.path}
+          title={
+            <>
+              {item.icon}
+              <span>{item.text}</span>
+            </>
+          }
+          icon={expandedItem === item.text ? <UpOutlined /> : <DownOutlined />}
+        >
+          {item.subItems &&
+            item.subItems.map((subItem, subIndex) => (
+              <Menu.Item key={subItem.path}>
+                <Link to={subItem.path}>{subItem.text}</Link>
+              </Menu.Item>
+            ))}
+        </Menu.SubMenu>
       ))}
-    </List>
+    </Menu>
   );
 };
 
@@ -159,14 +127,14 @@ interface MenuProps {
   componentToShow: React.ReactNode;
 }
 
-const Menu: React.FC<MenuProps> = ({ componentToShow }) => {
+const MenuComponent: React.FC<MenuProps> = ({ componentToShow }) => {
   const token = localStorage.getItem("role");
   const name = localStorage.getItem("name");
   const userRole = getUserRoleFromToken(token);
   const menuItems = getMenuItems(userRole);
   const { logout } = useAuth();
-  const [open] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
+  const [collapsed, setCollapsed] = useState(false); // Estado para colapsar o Sider
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -177,53 +145,66 @@ const Menu: React.FC<MenuProps> = ({ componentToShow }) => {
   }, []);
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-          backgroundColor: "#4caf50",
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            FCT Auto Test
-          </Typography>
-          <Typography sx={{ mr: 2 }}>{name}</Typography>
-          <Button onClick={logout} color="inherit" component={Link} to="/login">
-            Sair
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <div className="drawer-paper">
-          <MenuList menuItems={menuItems} />
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed}>
+        <div className="collapse-button-container">
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            className="collapse-button"
+          />
         </div>
-      </Drawer>
-      <Box component="main" className="main-content">
-        <Card
-          sx={{ overflowY: "auto", overflowX: "auto" }}
-          className="drawer-card"
-        >
-          <CardContent>
-            {isLoading ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "500px",
-                }}
+        <MenuList menuItems={menuItems} />
+      </Sider>
+      <Layout>
+        <Header style={{ backgroundColor: "#001529", color: "#FFFFFF" }}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography.Title level={3} style={{ color: "#FFFFFF" }}>
+            <img src={Logo} alt="" width="200px" style={{ marginTop: "13px", marginLeft:'-15px' }} />
+            </Typography.Title>
+            <div>
+              <SearchOutlinedIcon
+                style={{ marginRight: 25, color: "#FFFFFF" }}
+              />
+              <HelpOutlineOutlinedIcon
+                style={{ marginRight: 30, color: "#FFFFFF" }}
+              />
+
+              <NotificationsNoneOutlinedIcon
+                style={{ marginRight: 30, color: "#FFFFFF" }}
+              />
+              <Typography.Text style={{ marginRight: 16, color: "#FFFFFF" }}>
+                {name}
+              </Typography.Text>
+              <TranslateOutlinedIcon
+                style={{ marginRight: 1, color: "#FFFFFF" }}
+              />
+              {/* <Button
+                icon={<LogoutOutlined />}
+                type="primary"
+                onClick={logout}
+                href="/login"
               >
-                <CircularProgress />
-              </Box>
+                Sair
+              </Button> */}
+            </div>
+          </div>
+        </Header>
+        <Content style={{ margin: "16px" }}>
+          <Card style={{ overflow: "auto", minHeight: "500px" }}>
+            {isLoading ? (
+              <div className="loading-spinner">
+                <Spin size="large" />
+              </div>
             ) : (
               componentToShow
             )}
-          </CardContent>
-        </Card>
-      </Box>
-    </Box>
+          </Card>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
-export default Menu;
+export default MenuComponent;
